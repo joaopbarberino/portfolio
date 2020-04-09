c4ed9e7977e92542846cbe3cc5118cc4f0d7115b
João Barberino <joao.p.barberino@gmail.com>
2020-04-09 18:01:59 -0300

    Updates

---
+++ precache-manifest.def69837f3ce44b1bc7ed7113023f04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a49cf69f93d33463e99c500de2b841",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dbe2a3df8513edfc3681",
+    "url": "/portfolio/static/css/main.b2d44052.chunk.css"
+  },
+  {
+    "revision": "6bcfa3e06da95c1247f6",
+    "url": "/portfolio/static/js/2.f893ce79.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/portfolio/static/js/2.f893ce79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe2a3df8513edfc3681",
+    "url": "/portfolio/static/js/main.2e38a898.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
